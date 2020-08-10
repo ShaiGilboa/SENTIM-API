8dcc86619ce8f55545f7ac9f59b552244f2fed98
Shai Gilboa <shizel@gmail.com>
2020-08-09 22:40:58 -0400

    links

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -71,7 +71,7 @@ const App : React.FC<PropsWithChildren<props>> = () => {
           </AnimatePresence>
       </Content>
       <Footer>
-        <p>Created By: Shai Gilboa | <a href="https://github.com/ShaiGilboa" target="_blank">GitHub</a> | <a href="https://www.linkedin.com/in/shai-gilboa/" target="_blank">LinkedIn</a> | <a href="https://github.com/ShaiGilboa/SENTIM-API" target="_blank">Repo</a></p>
+        <p>Created By: Shai Gilboa | <a href="https://github.com/ShaiGilboa" target="_blank" rel="noopener noreferrer">GitHub</a> | <a href="https://www.linkedin.com/in/shai-gilboa/" target="_blank" rel="noopener noreferrer">LinkedIn</a> | <a href="https://github.com/ShaiGilboa/SENTIM-API" target="_blank" rel="noopener noreferrer">Repo</a></p>
       </Footer>
     </Wrapper>
   )
